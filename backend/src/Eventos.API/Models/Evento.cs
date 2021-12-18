97e06addfaabe9349bb43ce3832ef54068a08819
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2021-12-18 19:47:40 -0400

    Iniciando Projeto - Entity Framework

---
+++ backend/src/Eventos.API/Models/Evento.cs
@@ -0,0 +1,13 @@
+namespace Eventos.API.Models
+{
+        public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+    }
+}
